6b44fdd9842c37df3b7cad05c6fff8ffeb020a35
Ariane DELTOUR <ariane.deltour@gmail.com>
2019-03-06 09:07:26 -0500

    Liaison avec DB exterieur

--- OverDex/app/src/main/java/uqac/dim/overdex/DataBase/DatabaseHelper.java
+++ OverDex/app/src/main/java/uqac/dim/overdex/DataBase/DatabaseHelper.java
@@ -1,29 +1,103 @@
 package uqac.dim.overdex.DataBase;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private SQLiteDatabase myDataBase;
+    private final Context myContext;
+    private static String DATABASE_NAME = "overdex.db";
+    private static String DATABASE_PATH ="";
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "OverdexDB";
-
-    //private static final String CREATE_TABLE_HEROS;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        DATABASE_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        this.myContext = context;
+    }
+
+    public void createDatabase() throws IOException {
+
+        boolean dbExist = checkDataBase();
+        if (dbExist) {
+            Log.v("DIM", "DB exists");
+        } else {
+            Log.v("DIM", "DB not exists");
+            try {
+                this.getReadableDatabase();
+                this.close();
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");}
+        }
+    }
+
+    private boolean checkDataBase(){
+        boolean checkDB = false;
+        try{
+            File dbfile = new File(DATABASE_PATH + DATABASE_NAME);
+            checkDB = dbfile.exists();
+        }
+        catch(SQLiteException e){
+            Log.v("DIM", e.getMessage());
+        }
+        return checkDB;
+    }
+
+    private void copyDataBase() throws IOException{
+        OutputStream myOutput = new FileOutputStream(DATABASE_PATH + DATABASE_NAME);
+        InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+        myInput.close();
+        myOutput.flush();
+        myOutput.close();
+    }
+
+    public void db_delete(){
+        File file = new File(DATABASE_PATH + DATABASE_NAME);
+        if(file.exists()){
+            file.delete();
+            System.out.println("delete database file.");
+        }
+    }
+
+    public void openDatabase() throws SQLException {
+        String myPath = DATABASE_PATH + DATABASE_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public synchronized void closeDataBase() throws SQLException {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        //db.execSQL(CREATE_TABLE_HEROS);
-    }
+    public void onCreate(SQLiteDatabase db) {}
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_HEROES);
-        onCreate(db);
+        if (newVersion > oldVersion) {
+            db_delete();
+        }
     }
 }
--- OverDex/app/src/main/java/uqac/dim/overdex/MainActivity.java
+++ OverDex/app/src/main/java/uqac/dim/overdex/MainActivity.java
@@ -1,18 +1,39 @@
 package uqac.dim.overdex;
 
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
 import uqac.dim.overdex.DataBase.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper db;
+    DatabaseHelper dbHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = new DatabaseHelper(getApplicationContext());
+        dbHelp = new DatabaseHelper(getApplicationContext());
+
+        try {
+            dbHelp.createDatabase();
+        }
+        catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+
+        try {
+            dbHelp.openDatabase();
+            Log.v("DIM", "DB open");
+        }
+        catch(SQLException sqle){
+            throw sqle;
+        }
     }
 }
+
